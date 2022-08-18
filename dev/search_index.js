50bd1124aa2df8ab014e507e268ccc93163a9f82
Documenter.jl <documenter@juliadocs.github.io>
2022-08-18 17:09:18 +0000

    build based on 63b842c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ZeroHFTrader","category":"page"},{"location":"#ZeroHFTrader","page":"Home","title":"ZeroHFTrader","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ZeroHFTrader.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ZeroHFTrader]","category":"page"}]
+}
